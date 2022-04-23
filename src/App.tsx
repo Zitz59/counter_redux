951817e5a727020a157b84601cf34aee948ace7e
Zitz59 <dondongudon@gmail.com>
2022-04-24 00:50:28 +0300

    add Counter Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Counter} from './Counter';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [startValue, setStartValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(7)
+    const [counterValue, setCounterValue] = useState<number>(0)
+    const [isDisabledButton, setIsDisabledButton] = useState(false)
+
+
+
+    return (
+        <div className="App">
+            <Counter startValue={startValue}
+                     setStartValue={setStartValue}
+                     maxValue={maxValue}
+                     setMaxValue={setMaxValue}
+                     counterValue={counterValue}
+                     setCounterValue={setCounterValue}
+                     isDisabledButton={isDisabledButton}
+                     setIsDisabledButton={setIsDisabledButton}
+
+
+            />
+
+        </div>
+    );
 }
 
 export default App;
