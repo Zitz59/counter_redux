a89564bbde1ddc71531a040e1eda60e3d4acddf8
Zitz59 <dondongudon@gmail.com>
2022-04-24 17:20:20 +0300

    refactor and resolve issues with disabled in button

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -8,29 +8,20 @@ export type CounterType = {
     setMaxValue: (number: number) => void
     counterValue: number
     setCounterValue: (number: number) => void
-    isDisabledButton: boolean
-    setIsDisabledButton: (isDisabled: boolean) => void
 }
 
 export function Counter(props: CounterType) {
 
-    const onclickButtonHandler = () => {
-        props.setCounterValue(props.counterValue + 1)
-        if (props.counterValue === props.maxValue) {
-            props.setIsDisabledButton(true)
-        }
-        console.log(props.maxValue)
-
+    const incButtonHandler = () => {
+        props.setCounterValue(props.counterValue + 1)//counter increments value
     }
-
     const resetButtonHandler = () => {
-        props.setCounterValue(props.startValue)
-        props.setIsDisabledButton(false)
-    }
-    const fullOrEmptyOutput = `${props.counterValue === props.maxValue ? s.output_title_full : s.output_title_empty}`
-    const incButtonClass = `${props.counterValue === props.maxValue ? s.button_disabled : ''}`
-    const resetButtonClass = `${props.counterValue < props.maxValue ? s.button_disabled : ''}`
+        props.setCounterValue(props.startValue)//reset counter to 0
 
+    }
+    const fullOrEmptyOutput = `${props.counterValue === props.maxValue ? s.output_title_full : s.output_title_empty}`//check for color of count
+    const incButtonClass = `${props.counterValue === props.maxValue ? s.button_disabled : ''}`//Check for opacity increment button
+    const resetButtonClass = `${props.counterValue === 0 ? s.button_disabled : ''}`//Check for opacity reset button
 
     return (
         <div className={s.counter_body}>
@@ -38,14 +29,13 @@ export function Counter(props: CounterType) {
                 <span className={fullOrEmptyOutput}>{props.counterValue}</span>
             </div>
             <div className={s.button_block}>
-                <button disabled={props.isDisabledButton} className={incButtonClass}
-                        onClick={onclickButtonHandler}>incr
+                <button disabled={props.counterValue === props.maxValue} className={incButtonClass}
+                        onClick={incButtonHandler}>incr
                 </button>
-                <button disabled={!props.isDisabledButton} className={resetButtonClass}
+                <button disabled={props.counterValue === 0} className={resetButtonClass}
                         onClick={resetButtonHandler}>reset
                 </button>
             </div>
-
         </div>)
 }
 
