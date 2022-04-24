63637f227c3e187282256a15c466ecf22bb3f55a
Zitz59 <dondongudon@gmail.com>
2022-04-24 17:20:37 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React, {useState} from 'react';
 import {Counter} from './Counter';
 
-
 function App() {
 
-    const [startValue, setStartValue] = useState<number>(0)
-    const [maxValue, setMaxValue] = useState<number>(7)
-    const [counterValue, setCounterValue] = useState<number>(0)
-    const [isDisabledButton, setIsDisabledButton] = useState(false)
-
+    const [startValue, setStartValue] = useState<number>(0)//start value for counter
+    const [maxValue, setMaxValue] = useState<number>(7)//max value for counter
+    const [counterValue, setCounterValue] = useState<number>(0)//counter state
 
 
     return (
@@ -19,12 +16,8 @@ function App() {
                      setMaxValue={setMaxValue}
                      counterValue={counterValue}
                      setCounterValue={setCounterValue}
-                     isDisabledButton={isDisabledButton}
-                     setIsDisabledButton={setIsDisabledButton}
-
 
             />
-
         </div>
     );
 }
