0d05386e0cb2b5998c9100adcf36236802fac996
Zitz59 <dondongudon@gmail.com>
2022-06-01 19:38:34 +0300

    add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './App.module.css'
 import {Counter} from './Components/Counter/Counter';
 import {Settings} from './Components/Settings/Settings';
@@ -6,13 +6,34 @@ import {Settings} from './Components/Settings/Settings';
 function App() {
 
     const [startValue, setStartValue] = useState<number>(0)//start value for counter
-    const [maxValue, setMaxValue] = useState<number>(7)//max value for counter
+    const [maxValue, setMaxValue] = useState<number>(0)//max value for counter
     const [counterValue, setCounterValue] = useState<number>(0)//counter state
 
 
+//создать callback который передаёт startValue в counterValue
+
+
+    useEffect(()=>{
+        let startValueAsString = localStorage.getItem('startValue')
+        let maxValueAsString = localStorage.getItem('maxValue')
+        startValueAsString && setStartValue(JSON.parse(startValueAsString))
+        maxValueAsString && setMaxValue(JSON.parse(maxValueAsString))
+    }, [])
+
+    useEffect(() => {
+            localStorage.setItem('startValue', JSON.stringify(startValue))
+            localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        }, [startValue,maxValue])
+
     return (
         <div className={s.app_wrapper}>
-            <Settings />
+            <Settings
+                startValue={startValue}
+                setStartValue={setStartValue}
+                maxValue={maxValue}
+                setMaxValue={setMaxValue}
+
+            />
 
             <Counter startValue={startValue}
                      setStartValue={setStartValue}
@@ -22,6 +43,7 @@ function App() {
                      setCounterValue={setCounterValue}
 
             />
+
 
         </div>
     );
