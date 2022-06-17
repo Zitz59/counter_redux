100f15c1e0ccf2a49f03f9e01b1a0fb9e06ba862
Zitz59 <dondongudon@gmail.com>
2022-06-17 10:26:22 +0300

    add logic and styles to incButton/resetButton

--- src/Components/Counter/Counter.tsx
+++ src/Components/Counter/Counter.tsx
@@ -8,15 +8,19 @@ export type CounterType = {
     setMaxValue: (number: number) => void
     counterValue: number
     setCounterValue: (number: number) => void
+    maxCounterValue:number
+    setMaxCounterValue:(number:number)=>void
 }
 
 export function Counter(props: CounterType) {
 
     const incButtonHandler = () => {
-        props.setCounterValue(props.counterValue + 1)//counter increments value
+        props.startValue <= props.maxValue ?
+        props.setCounterValue(props.counterValue + 1) : 's.button_disabled'//counter increments value
     }
     const resetButtonHandler = () => {
-        props.setCounterValue(props.startValue)//reset counter to 0
+        props.setCounterValue(props.startValue)//reset counter to startValue
+        props.setMaxCounterValue(props.maxValue)
 
     }
     const fullOrEmptyOutput = `${props.counterValue === props.maxValue ? s.output_title_full : s.output_title_empty}`//check for color of count
