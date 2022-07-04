647c339423bd767448886d1f6515703f027cf0f2
Zitz59 <dondongudon@gmail.com>
2022-07-04 13:31:24 +0300

    add callbacks and props

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,37 @@ import {Settings} from './Components/Settings/Settings';
 function App() {
 
     const [startValue, setStartValue] = useState<number>(0)//start value for counter
-    const [maxValue, setMaxValue] = useState<number>(0)//max value for counter
+    const [maxValue, setMaxValue] = useState<number>(1)//max value for counter
     const [counterValue, setCounterValue] = useState<number>(0)//counter state
+    const [maxCounterValue, setMaxCounterValue] = useState<number>(0)//maxCounter value for resetBtn
+    const [message, setMessage] = useState<string>('')
 
+    //const error = startValue || maxValue < 0 || startValue === maxValue
+    //console.log(error)
 
 //создать callback который передаёт startValue в counterValue
 
+    useEffect(() => {
 
-    useEffect(()=>{
         let startValueAsString = localStorage.getItem('startValue')
         let maxValueAsString = localStorage.getItem('maxValue')
         startValueAsString && setStartValue(JSON.parse(startValueAsString))
         maxValueAsString && setMaxValue(JSON.parse(maxValueAsString))
     }, [])
 
-    useEffect(() => {
-            localStorage.setItem('startValue', JSON.stringify(startValue))
-            localStorage.setItem('maxValue', JSON.stringify(maxValue))
-        }, [startValue,maxValue])
+    //синхронизация должна запускаться каждый раз когда мы жмём кнопку SET
+    //либо когда меняем min\max values в SETTINGS => попадает в  localState
 
     return (
         <div className={s.app_wrapper}>
             <Settings
+                setMessage={setMessage}
                 startValue={startValue}
                 setStartValue={setStartValue}
                 maxValue={maxValue}
                 setMaxValue={setMaxValue}
-
+                setCounterValue={setCounterValue}
+                setMaxCounterValue={setMaxCounterValue}
             />
 
             <Counter startValue={startValue}
@@ -41,10 +45,10 @@ function App() {
                      setMaxValue={setMaxValue}
                      counterValue={counterValue}
                      setCounterValue={setCounterValue}
-
+                     maxCounterValue={maxCounterValue}
+                     setMaxCounterValue={setMaxCounterValue}
+                     message={message}
             />
-
-
         </div>
     );
 }
