9bc3a45c60f33a50c357b4b89cf2e61c7c259fc1
Zitz59 <dondongudon@gmail.com>
2022-07-18 14:15:22 +0300

    absolutely universal button

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
+import React, {ButtonHTMLAttributes} from 'react';
 
 type ButtonPropsType = {
-    className: string
     buttonName: string
-    callBack: () => void
-    disabled: boolean
 }
 
-
-export const Button = (props: ButtonPropsType) => {
-    const OnClickHAndler = props.callBack
+export const Button = ({buttonName, ...props}: ButtonPropsType & ButtonHTMLAttributes<any>) => {
 
     return (
 
-        <button className={props.className} disabled={props.disabled} onClick={OnClickHAndler}>{props.buttonName}</button>
+        <button {...props}>{buttonName}</button>
     )
 }
-
-// КАК НИБУДЬ ПОДКЛЮЧИТЬ СТИЛИ К ЭТОЙ ЕБУЧЕЙ КНОПКЕ, ОНИ НЕ РАБОТАЮТ
