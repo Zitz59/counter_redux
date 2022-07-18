389cfa6a47b9f808d3549ce4c3aa36d3bf6ae59a
Zitz59 <dondongudon@gmail.com>
2022-07-18 14:14:41 +0300

    refactor and add destructuring props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import s from './App.module.css'
 import {Counter} from './Components/Counter/Counter';
 import {Settings} from './Components/Settings/Settings';
@@ -10,35 +10,15 @@ function App() {
     const [counterValue, setCounterValue] = useState<number>(0)//counter state
     const [message, setMessage] = useState<string>('')
 
-    // let counterError = counterValue < 0 ? setError(!error): null
-    //     || startValue < 0 ?  setError(!error) : null
-    //     || maxValue <= 0 ? setError(!error) : null
-    //     || startValue >= maxValue ? setError(!error)  : null
-    //     || counterValue > maxValue ? setError(!error)  : null
-    // localStorage.setItem('counterError',JSON.stringify(counterError))
-
-
-    //const error = startValue || maxValue < 0 || startValue === maxValue
-    //console.log(error)
-
-//создать callback который передаёт startValue в counterValue
 
     useEffect(() => {
         let startValueAsString = localStorage.getItem('startValue')
         let maxValueAsString = localStorage.getItem('maxValue')
-        // let counterValueAsString = localStorage.getItem('counterValue')
-        // let counterErrorAsString = localStorage.getItem('counterError')
+        let counterValueAsString = localStorage.getItem('counterValue')
         startValueAsString && setStartValue(JSON.parse(startValueAsString))
         maxValueAsString && setMaxValue(JSON.parse(maxValueAsString))
-        // counterValueAsString && setCounterValue(JSON.parse(counterValueAsString))
-        // counterErrorAsString && setError(JSON.parse(counterErrorAsString))
-    }, [])
-
-
-    useEffect(()=>{
-        let counterValueAsString = localStorage.getItem('counterValue')
         counterValueAsString && setCounterValue(JSON.parse(counterValueAsString))
-    },[])
+    }, [])
 
     //синхронизация должна запускаться каждый раз когда мы жмём кнопку SET
     //либо когда меняем min\max values в SETTINGS => попадает в  localState
