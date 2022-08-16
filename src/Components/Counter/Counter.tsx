a00e341fdfaced044a39a2050e647822ca8d4146
Zitz59 <dondongudon@gmail.com>
2022-08-16 15:28:20 +0300

    feat: add redux refactor:change variables, delete unused props

--- src/Components/Counter/Counter.tsx
+++ src/Components/Counter/Counter.tsx
@@ -1,46 +1,43 @@
 import React from 'react';
 import s from './Counter.module.css';
+import {incCounterAC, resetCounterAC} from '../../redux/counterReducer';
+import {useAppDispatch, useAppSelector} from '../../redux/hooks';
 
 export type CounterType = {
-    startValue: number
-    setStartValue: (number: number) => void
-    maxValue: number
-    setMaxValue: (number: number) => void
-    counterValue: number
-    setCounterValue: (number: number) => void
     message: string
     setMessage: (value: string) => void
 
 }
-
 export function Counter(props: CounterType) {
-    const {setCounterValue, startValue, counterValue, message, maxValue} = props
+    const {message} = props
+    const counterValue = useAppSelector<number>(state => state.counter.counterValue)
+    const minValue = useAppSelector<number>(state => state.counter.minValue)
+    const maxValue = useAppSelector<number>(state => state.counter.maxValue)
+    const dispatch = useAppDispatch()
 
     let counterError = counterValue < 0
-        || startValue < 0
+        || minValue < 0
         || maxValue <= 0
-        || startValue >= maxValue
+        || minValue >= maxValue
         || counterValue > maxValue
 
     const incButtonHandler = () => {
         if (counterValue === maxValue || counterValue < maxValue) {
-            return setCounterValue(counterValue + 1)
+            return dispatch(incCounterAC())
         } else {
             return s.button_disabled
         }
     }
     const resetButtonHandler = () => {
-        setCounterValue(maxValue)//reset counter to startValue
-        setCounterValue(startValue)
+        dispatch(resetCounterAC())
     }
 
     const fullOrEmptyOutputClass = `${counterValue === maxValue ? s.output_title_full : s.output_title_empty} `
     const incButtonClass = `${counterValue === maxValue || message ? s.button_disabled : ''}`// добавить условие для enterValue
     const resetButtonClass = `${message ? s.button_disabled : ''}`
-
     const errorText = 'Please enter correct value!'
     const enterValueText = 'Enter the value and press Set'
-    const counterOutput = <div>{props.counterValue}</div>
+    const counterOutput = <div>{counterValue}</div>
     const errorOutput = <div className={s.spanError}>{errorText}</div>
     const enterTextOutput = <div className={s.enterValue}>{enterValueText}</div>
     const finalOutput = message === enterValueText ? enterTextOutput
@@ -56,7 +53,7 @@ export function Counter(props: CounterType) {
                 <button disabled={counterValue === maxValue || !!message} className={incButtonClass}
                         onClick={incButtonHandler}>incr
                 </button>
-                <button disabled={counterError || startValue < 0 || !!message} className={resetButtonClass}
+                <button disabled={counterError || minValue < 0 || !!message} className={resetButtonClass}
                         onClick={resetButtonHandler}>reset
                 </button>
             </div>
