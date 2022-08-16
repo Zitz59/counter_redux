bd62a46d0899fa85ccfed16a12369af516803a12
Zitz59 <dondongudon@gmail.com>
2022-08-16 15:35:49 +0300

    feat: change variables, add dispatches from thunk refactor:delete unused imports

--- src/Components/Settings/Settings.tsx
+++ src/Components/Settings/Settings.tsx
@@ -1,53 +1,53 @@
 import {ChangeEvent} from 'react';
 import s from './Settings.module.css'
 import {Button} from '../Button/Button';
+import {
+    incValuesTC,
+    setLocalStorageMaxValueAC,
+    setLocalStorageMinValueAC,
+} from '../../redux/counterReducer';
+import {useAppDispatch, useAppSelector} from '../../redux/hooks';
 
-export type SettingsType = {
-    startValue: number
-    setStartValue: (newStartValue: number) => void
-    maxValue: number
-    setMaxValue: (newMaxValue: number) => void
-    setCounterValue: (number: number) => void
-    setMessage: (value: string) => void
-
-}
+export type SettingsType = { setMessage: (value: string) => void }
 
 export function Settings(props: SettingsType) {
-    const {maxValue, startValue, setMessage, setMaxValue, setCounterValue, setStartValue} = props
-    //сетаем кнопкой и значения отправляются в  localStorage
+
+    const {setMessage} = props
+    const maxValue = useAppSelector<number>(state => state.counter.maxValue)
+    const minValue = useAppSelector<number>(state => state.counter.minValue)
+    const dispatch = useAppDispatch()
+
+    //---SET-TO-LOCAL-STORAGE----
     const setButtonHandler = () => {
-        setCounterValue(startValue)
-        localStorage.setItem('startValue', JSON.stringify(startValue))
-        localStorage.setItem('maxValue', JSON.stringify(maxValue))
-        localStorage.setItem('counterValue', JSON.stringify(startValue))
+        dispatch(incValuesTC())
         setMessage('')
     }
 
-//  хапаем значения из MinValue и parse to integer => суём в UseState
     const onChangeMinValue = (e: ChangeEvent<HTMLInputElement>) => {
-        let newStartValue = e.currentTarget.value
-        setStartValue(parseInt(newStartValue))
-        if ((parseInt(newStartValue)) < 0) {
+        let minValue = parseInt(e.currentTarget.value)
+        dispatch(setLocalStorageMinValueAC(minValue))
+        if (minValue < 0) {
             return setMessage('Please enter correct value!')
-        }
-        if ((parseInt(newStartValue)) >= maxValue) {
+        }if (minValue >= maxValue) {
             return setMessage('Please enter correct value!')
-        } else setMessage('Enter the value and press Set')
+        } else  setMessage('Enter the value and press Set')
     }
-//  хапаем значения из  maxValue и parse to integer => суём в UseState
+
     const onChangeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
-        let newMaxValue = e.currentTarget.value
-        setMaxValue(parseInt(newMaxValue))
-        if ((parseInt(newMaxValue)) <= 0) {
+        let maxValue = parseInt(e.currentTarget.value)
+        dispatch(setLocalStorageMaxValueAC(maxValue))
+        if (maxValue <= 0) {
             return setMessage('Please enter correct value!')
-        }
-        if (startValue >= (parseInt(newMaxValue))) {
+        }if (minValue >= maxValue) {
+            return setMessage('Please enter correct value!')
+        }if (minValue < 0 && maxValue > 0) {
             return setMessage('Please enter correct value!')
         } else setMessage('Enter the value and press Set')
     }
 
-    let settingsError = startValue >= maxValue
-        || startValue < 0
+    let settingsError =
+        minValue >= maxValue
+        || minValue < 0
         || maxValue <= 0
 
     const setButtonClass = settingsError ? s.button_disabled : s.button_enabled
@@ -63,7 +63,7 @@ export function Settings(props: SettingsType) {
                 </div>
                 <div className={s.input_block}>
                     <label className={s.input_title}>min value :</label>
-                    <input value={startValue} onChange={onChangeMinValue} className={inputValueError}
+                    <input value={minValue} onChange={onChangeMinValue} className={inputValueError}
                            type="number"/>
                 </div>
             </div>
