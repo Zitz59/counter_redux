b7978c6b252a10b5bb611146265faa5123f027bd
Zitz59 <dondongudon@gmail.com>
2022-08-16 15:29:02 +0300

    refactor: delete unused props,delete useState functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,24 @@ import {useEffect, useState} from 'react';
 import s from './App.module.css'
 import {Counter} from './Components/Counter/Counter';
 import {Settings} from './Components/Settings/Settings';
+import {useAppDispatch} from './redux/hooks';
+import {setValuesTC} from './redux/counterReducer';
 
 function App() {
-
-    const [startValue, setStartValue] = useState<number>(0)//start value for counter
-    const [maxValue, setMaxValue] = useState<number>(1)//max value for counter
-    const [counterValue, setCounterValue] = useState<number>(0)//counter state
     const [message, setMessage] = useState<string>('')
+    const dispatch=useAppDispatch()
 
-
+    //----SETFROMLOCALSTORAGE-----
     useEffect(() => {
-        let startValueAsString = localStorage.getItem('startValue')
-        let maxValueAsString = localStorage.getItem('maxValue')
-        let counterValueAsString = localStorage.getItem('counterValue')
-        startValueAsString && setStartValue(JSON.parse(startValueAsString))
-        maxValueAsString && setMaxValue(JSON.parse(maxValueAsString))
-        counterValueAsString && setCounterValue(JSON.parse(counterValueAsString))
+        dispatch(setValuesTC())
     }, [])
-
-    //синхронизация должна запускаться каждый раз когда мы жмём кнопку SET
-    //либо когда меняем min\max values в SETTINGS => попадает в  localState
 
     return (
         <div className={s.app_wrapper}>
-            <Settings
+            <Settings setMessage={setMessage}/>
+            <Counter
                 setMessage={setMessage}
-                startValue={startValue}
-                setStartValue={setStartValue}
-                maxValue={maxValue}
-                setMaxValue={setMaxValue}
-                setCounterValue={setCounterValue}
-            />
-
-            <Counter startValue={startValue}
-                     setStartValue={setStartValue}
-                     maxValue={maxValue}
-                     setMaxValue={setMaxValue}
-                     counterValue={counterValue}
-                     setCounterValue={setCounterValue}
-                     setMessage={setMessage}
-                     message={message}
-
-            />
+                     message={message}/>
         </div>
     );
 }
